2f4f57fd5f44d3beaa253bddca3720a6e89013cf
YURISA IKAWA <yurisa19861227@gmail.com>
2019-05-22 14:49:55 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ def setup
   @base_title = "Ruby on Rails Tutorial Sample App"
 end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -19,6 +24,12 @@ end
   end
 
   test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
