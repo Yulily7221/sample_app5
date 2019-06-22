aec7ac79094cb15ca27aab956d5ebdcd40bcb3af
YURISA IKAWA <yurisa19861227@gmail.com>
2019-06-22 09:23:59 +0000

    6/22

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,10 +31,13 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-    #duplicate_user = @user.dup
-    #duplicate_user.email = @user.email.upcase
-    #@user.save
-    #assert_not duplicate_user.valid?
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+    test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
